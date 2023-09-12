8fced4c0a38e5078a2cf93ee9b90a8c34d7de91f
ersanputra <ersancakra@gmail.com>
2023-09-12 20:34:22 +0700

    push ke git

---
+++ services/user.service.js
@@ -0,0 +1,55 @@
+const UserModel = require("../models/user.model");
+
+class UserService {
+  constructor() {
+    this.userModel = new UserModel();
+  }
+
+  async findByEmail(inputEmail) {
+    const email = await this.userModel.findByEmail(inputEmail);
+    return email;
+  }
+
+  async registration(payload) {
+    try {
+      // 1 validasi user terdaptar atau belum
+      const { email } = payload;
+      //console.log(email);
+      const user = await this.findByEmail(email);
+      //console.log(user);
+      if (user) {
+        throw new Error("User sudah terdaptar!")
+      }
+
+      // 2 store data request to db
+      const userNew = new UserModel(payload.id, payload.email, payload.password);
+      await this.userModel.save(userNew);
+
+      return "User berhasil di simpan!"
+   } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * ini service login
+   * @param {*} payload 
+   */
+  async login(payload) {
+    const { email } = payload;
+    const { password } = payload;
+    const user = await this.findByEmail(email);
+    // 1 validasi email, email terdaftar atau tidak, kalau tidak errorkan user tidak terdaftar.
+    if (!user) {
+      throw new Error("User tidak ditemukan"  + email + password)
+      // 2 check password, kalau sama sukses kalau beda trow error password tidak sesuai
+    } else if (password!=user.password){
+      throw new Error ("Password salah !!" + email + password)
+    }
+    // 3 kasih message berhasil login ketika email dan password berhasil. 
+    return ("Anda berhasil log-in")
+
+  }
+}
+
+module.exports = UserService;
